d07303670f38c2c3dce2f20138124c81717fb7b5
dashaSU32 <suhanovadasha767@gmail.com>
2025-05-04 17:21:57 +0700

    Add files via upload

---
+++ ping pong.py
@@ -0,0 +1,28 @@
+from pygame import*
+# класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+ # конструктор класса
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       # Вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
+ 
+       
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+ 
+       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ # метод, отрисовывающий героя на окне
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+# класс главного игрока
+class Player(GameSprite):
+   # метод для управления спрайтом стрелками клавиатуры
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
